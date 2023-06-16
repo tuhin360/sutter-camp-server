f88bc8f9d3969ba7e59fdacc2d23d716f774973f
tuhin <tuhin153695@gmail.com>
2023-06-16 17:12:42 +0600

    get user api added

--- index.js
+++ index.js
@@ -43,8 +43,19 @@ async function run() {
 
 
     // user api
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/users', async(req, res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(user);
+      if (existingUser){
+        return res.send({message: 'user already exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
